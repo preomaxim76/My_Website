8934ca12f2923d7c50dbae1237898cc24c8cdf16
preomaxim <preomaxim76@gmail.com>
2026-07-12 15:08:07 +0400

    First upload of my website files

---
+++ script.js
@@ -0,0 +1,27 @@
+// Navbar
+
+fetch('navbar.html')
+    .then(response => response.text())
+    .then (data => {
+        document.getElementById("nav-placeholder").innerHTML = data;
+
+        let links = document.querySelectorAll(".link"); 
+
+        for (link of links)
+        {
+            link.addEventListener("click", underline_link);
+        }
+    });
+
+function underline_link(event)
+{
+    let links = document.querySelectorAll(".link"); 
+
+    for (link of links)
+    {
+        link.classList.remove("clicked")
+    }
+    event.preventDefault();
+    this.classList.add("clicked");
+}
+
