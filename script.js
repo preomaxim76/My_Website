fb3082108c5071967d496626cb5b34e8fa7ecef6
preomaxim <preomaxim76@gmail.com>
2026-07-16 22:37:41 +0400

    Added glowing button

--- script.js
+++ script.js
@@ -24,18 +24,6 @@ else
             link.addEventListener("click", underline_link);
         }
     });
-
-    function underline_link(event)
-    {
-    let links = document.querySelectorAll(".link"); 
-
-    for (link of links)
-    {
-        link.classList.remove("clicked")
-    }
-    event.preventDefault();
-    this.classList.add("clicked");
-}
 }
 
 
